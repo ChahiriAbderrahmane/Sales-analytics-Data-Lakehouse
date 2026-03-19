365c44e8aa9ad64073b6bc96a7e74b355189db52
Chahiri Abderrahmane <chahiri.abderrahmane@ine.inpt.ac.ma>
2026-03-19 18:03:23 +0000

    removed critical infos

--- export_aggregates_from_hdfs_to_s3.py
+++ export_aggregates_from_hdfs_to_s3.py
@@ -4,8 +4,8 @@ spark = SparkSession.builder \
     .appName("Export Aggregates to S3") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-    .config("spark.hadoop.fs.s3a.access.key", "***REMOVED***") \
-    .config("spark.hadoop.fs.s3a.secret.key", "***REMOVED***") \
+    .config("spark.hadoop.fs.s3a.access.key", "YourKey") \
+    .config("spark.hadoop.fs.s3a.secret.key", "YourSecretKey") \
     .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
